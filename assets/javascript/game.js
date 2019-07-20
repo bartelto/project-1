a6d2ae8bca17f3279e3f4aed2da85cc29609bf79
Miles Standoffish <toddnan@gmail.com>
2019-07-20 08:53:34 -0500

    added chat area and avatar placeholders

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 setCardValue("#player-card", "0H");
 setCardValue("#opponent-card", "AD");
-setCardValue("#player-wild-1", "8C");
-setCardValue("#player-wild-2", "JS");
+//setCardValue("#player-wild-1", "8C");
+//setCardValue("#player-wild-2", "JS");
 
 function setCardValue(cardId, cardCode) {
     // set rank
@@ -12,10 +12,18 @@ function setCardValue(cardId, cardCode) {
     }
 
     // set suit
+    $(cardId).removeClass("spade club heart diamond");
     switch (cardCode.substring(1)) {
-        case "S": {$(cardId).addClass("card spade");} break;
-        case "C": {$(cardId).addClass("card club");} break;
-        case "H": {$(cardId).addClass("card heart");} break;
-        default: {$(cardId).addClass("card diamond");} 
+        case "S": {$(cardId).addClass("playing-card spade");} break;
+        case "C": {$(cardId).addClass("playing-card club");} break;
+        case "H": {$(cardId).addClass("playing-card heart");} break;
+        default: {$(cardId).addClass("playing-card diamond");} 
     }
 }
+
+$("#player-deck").on("click", function() {
+    // play the next card (demo only)
+    let randomCards = ["8S", "9H", "AC", "KD", "3S", "7H", "AC"];
+    let randomCard = randomCards[Math.floor(Math.random()*randomCards.length)];
+    setCardValue("#player-card", randomCard);
+});

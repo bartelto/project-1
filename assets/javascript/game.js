c4739410bcf603689a15316e0d4300f5b25d2c11
Sunny <loganayers2013@yahoo.com>
2019-07-20 10:47:41 -0500

    added the firebase link

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,16 @@
+//Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyDFbSU7PZJHTzMVbA0JE3bet5RWadB2ajc",
+    authDomain: "test-dc061.firebaseapp.com",
+    databaseURL: "https://test-dc061.firebaseio.com",
+    projectId: "test-dc061",
+    storageBucket: "",
+    messagingSenderId: "723834254447",
+    appId: "1:723834254447:web:b99d118c96d67216"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 setCardValue("#player-card", "0H");
 setCardValue("#opponent-card", "AD");
 //setCardValue("#player-wild-1", "8C");
@@ -14,16 +27,34 @@ function setCardValue(cardId, cardCode) {
     // set suit
     $(cardId).removeClass("spade club heart diamond");
     switch (cardCode.substring(1)) {
-        case "S": {$(cardId).addClass("playing-card spade");} break;
-        case "C": {$(cardId).addClass("playing-card club");} break;
-        case "H": {$(cardId).addClass("playing-card heart");} break;
-        default: {$(cardId).addClass("playing-card diamond");} 
+        case "S": { $(cardId).addClass("playing-card spade"); } break;
+        case "C": { $(cardId).addClass("playing-card club"); } break;
+        case "H": { $(cardId).addClass("playing-card heart"); } break;
+        default: { $(cardId).addClass("playing-card diamond"); }
     }
 }
 
-$("#player-deck").on("click", function() {
+$("#player-deck").on("click", function () {
     // play the next card (demo only)
     let randomCards = ["8S", "9H", "AC", "KD", "3S", "7H", "AC"];
-    let randomCard = randomCards[Math.floor(Math.random()*randomCards.length)];
+    let randomCard = randomCards[Math.floor(Math.random() * randomCards.length)];
     setCardValue("#player-card", randomCard);
 });
+
+// Game Logic
+function startGame() {
+    var queryURL = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+    console.log(this);
+
+    $("#player-deck").on("click", function () {
+        // when player deck is clicked
+        "https://deckofcardsapi.com/api/deck/<<deck_id>>/pile/<<pile_name>>/draw/?count=2"
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+
+        });
+    });
+}
